63ac11e2dfd21ab4c829b6a0f54ced2f66fde2b4
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-12-11 11:15:20 +0100

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using EasyRpc.DynamicClient;
+using EasyRpc.DynamicClient.Grace;
+using Grace.DependencyInjection;
+using SampleWebApp.Services;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c => c.Export<InterfaceNamingConvention>().As<INamingConventionService>());
+
+            container.ProxyNamespace("https://localhost:44307/", namespaces: typeof(ICalculatorService).Namespace);
+
+            var service = container.Locate<ICalculatorService>();
+            var result = service.Add(2, 2);
+            Console.WriteLine($"The result is {result}");
+            Console.ReadLine();
+        }
+    }
+
+    internal class InterfaceNamingConvention : INamingConventionService
+    {
+        public string GetNameForType(Type type)
+        {
+            return type.Name.Substring(1);
+        }
+
+        public string GetMethodName(MethodInfo method)
+        {
+            return method.Name;
+        }
+    }
+}
---
+++ SampleWebApp/Services/CalculatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApp.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+
+    public interface ICalculatorService
+    {
+        double Add(double a, double b);
+    }
+}
